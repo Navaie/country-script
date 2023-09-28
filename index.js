5d993922c35798c2c789fb45fbdb669ae91e2dff
Robin van Rijn <robin@stippl.io>
2023-09-28 14:36:46 +0200

    geocode update

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const lookup = require("country-code-lookup");
+const { lookUp: lookUpCountry } = require("geojson-places");
 
 console.log("Lookup");
 console.log(lookup.byCountry("United Kingdom"));
+console.log(lookUpCountry(50.05631, 14.459095));
